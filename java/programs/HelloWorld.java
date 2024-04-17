008b6bf13eb9bd9f3edf247f579dfaa3bb7b81e1
Rahul Dabgotra <rahuldabgo@gmail.com>
2024-04-18 01:51:27 +0530

    Update HelloWorld.java

--- java/programs/HelloWorld.java
+++ java/programs/HelloWorld.java
@@ -1,13 +1,5 @@
 /**
- * Program Name: HelloWorld
- * Description: ...
- * 
- * To compile and run this program:
- * 1. Save this file as HelloWorld.java
- * 2. Open a terminal or command prompt.
- * 3. Navigate to the directory containing this file.
- * 4. Compile the program using the command: javac -d output HelloWorld.java
- * 5. Run the program using the command: java HelloWorld
+ * HelloWorld
  */
 public class HelloWorld {
     public static void main(String[] args) {
